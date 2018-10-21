cd70275c82167255e9d11e5c8756fb678dba16d4
AgentAileron <rrjayaram@outlook.com.au>
2018-10-22 00:34:02 +1100

    Initialise files to begin coding bot

--- code/Program.cs
+++ code/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿/* MAIN PROGRAM FOR DOG-E Node VII
+ * AgentAileron 2018
+ * LM: 22-10-2018
+ */
 
-namespace code
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace code{
+    class Program{
+        static void Main(string[] args){
             Console.WriteLine("Hello World!");
+        }
+
+        static async Task MainAsync(string[] args){
+            
         }
     }
 }
