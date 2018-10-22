7f71499a4e6b710b3b4179a9fe0eee8c4b1dfd55
AgentAileron <rrjayaram@outlook.com.au>
2018-10-22 14:53:54 +1100

    Update gitignore, add iterative progress

--- code/Program.cs
+++ code/Program.cs
@@ -4,17 +4,47 @@
  */
 
 using System;
+using System.IO;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using DSharpPlus;
 
+
 namespace code{
     class Program{
+
+        static string auth_token;
+
+        // == INITIALISATION == //
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+
+            // -- Attempt to load in the auth token from file, halt execution otherwise --
+            try{
+                System.IO.StreamReader f = new System.IO.StreamReader(@"./auth_token.txt");
+                auth_token = f.ReadLine();
+            }catch (Exception e){
+                // FileNotFound Exception
+                if (e is FileNotFoundException){
+                    Console.WriteLine("Error loading auth token: auth_token.txt does not exist!");
+                    Environment.Exit(2);
+                }
+                // Unexpected exception type
+                else throw;
+            }
+
+            // Print a message on successful initialisation
+            Console.WriteLine("-//- DOG-E Initialised successfully! -//-");
+            Console.WriteLine("Auth token: " + auth_token);
         }
 
+        // == ASYNC EXECUTION == //
         static async Task MainAsync(string[] args){
-            
+            // Initialise bot
+            DiscordClient bot = new DiscordClient(new DiscordConfiguration{
+                Token = auth_token,
+                TokenType = TokenType.Bot
+            });
         }
+
     }
 }
